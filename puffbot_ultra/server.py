43734e3dc0a362d98bea80de15d8b4af6d0c8ccc
slurpinpuffs <swirlinlix@gmail.com>
2024-08-21 12:19:48 -0700

    Added docstrings.

--- puffbot_ultra/server.py
+++ puffbot_ultra/server.py
@@ -1,5 +1,6 @@
 class Server:
     def __init__(self, server_id, yt_channels=None, update_channel_id=None):
+        """Initialize server object with Discord server ID."""
         self.server_id = server_id
         if yt_channels is None:
             self.yt_channels = {}  # Instantiate empty dict
@@ -8,15 +9,17 @@ class Server:
         self.update_channel_id = update_channel_id  # Which channel to post in
     
     def set_update_channel_id(self, discord_channel_id):
+        """Set Discord channel where updates will be sent."""
         self.update_channel_id = discord_channel_id
     
     def add_yt_channel(self, channel_name):
-        # Adds new yt channel to channel list and initializes latest community post
+        """Add new YT channel to yt_channels dict and initializes the latest community post."""
         self.yt_channels[channel_name] = ""
 
     def remove_yt_channel(self, channel_name):
+        """Remove given channel from yt_channels dict."""
         self.yt_channels.pop(channel_name)
 
     def set_recent_post(self, channel_name, recent_post_id):
-        # Saves new recent post to dict
+        """Save new recent post to yt_channels dict."""
         self.yt_channels[channel_name] = recent_post_id
--- puffbot_ultra/server_list.py
+++ puffbot_ultra/server_list.py
@@ -1,21 +1,24 @@
 from puffbot_ultra.server import Server
 import json
 
-SAVE_PATH = "./servers_save.txt"
+SAVE_PATH = "./servers_save.json"
 
 
 class ServerList:
     def __init__(self):
+        """Initialize list of the bot's Discord servers."""
         self.servers = []  # List of saved Server objects
         self.load()
 
     def get_server_by_id(self, server_id):
+        """Return Discord server with given server ID."""
         for server in self.servers:
             if server.server_id == server_id:
                 return server
         return -1
     
     def add_server(self, new_server_id):
+        """Add new Discord server to server list."""
         for server in self.servers:
             if server == new_server_id:
                 pass
@@ -24,9 +27,11 @@ class ServerList:
         self.servers.append(new_server)
 
     def remove_server(self, server_id):
+        """Remove Discord server from server list."""
         self.servers.remove(server_id)
 
     def load(self):
+        """Load server list from json file."""
         try:
             f = open(SAVE_PATH, "r")
             save_json = f.read()
@@ -38,5 +43,6 @@ class ServerList:
             print("Failed to load saved server list.")
 
     def save(self):
+        """Save server list to json file."""
         with open(SAVE_PATH, "w") as file:
             json.dump([server.__dict__ for server in self.servers], file)
